11ea4231f73157aab306a784e3bb7bde1fcfa47b
Daniel Keller <daniel.keller@yale.edu>
2016-11-29 16:13:18 -0500

    Hard coded in stocks

--- predict_my_money/computations/recommender_interface.py
+++ predict_my_money/computations/recommender_interface.py
@@ -12,8 +12,48 @@ from predict_my_money.computations.recommender_algorithms import recommend_rando
 
 #backend imports
 import datetime
+import json
 from predict_my_money.utils import stockDayDatabaseInterface
-from predict_my_money.models import Portfolio
+from django.http import HttpResponse
+
+class SandP():
+    def __init__(self):
+        self.stocks = [
+            "MMM", "ABT", "ABBV", "ACN", "ATVI", "AYI", "ADBE", "AAP", "AES", "AET", "AMG", "AFL", "A", "APD", "AKAM",
+            "ALK", "ALB", "AA", "ALXN", "ALLE", "AGN", "ADS", "LNT", "ALL", "GOOGL", "GOOG", "MO", "AMZN", "AEE", "AAL",
+            "AEP", "AXP", "AIG", "AMT", "AWK", "AMP", "ABC", "AME", "AMGN", "APH", "APC", "ADI", "ANTM", "AON", "APA",
+            "AIV", "AAPL", "AMAT", "ADM", "AJG", "AIZ", "T", "ADSK", "ADP", "AN", "AZO", "AVGO", "AVB", "AVY", "BHI",
+            "BLL", "BAC", "BCR", "BAX", "BBT", "BDX", "BBBY", "BRK-B", "BBY", "BIIB", "BLK", "HRB", "BA", "BWA", "BXP",
+            "BSX", "BMY", "BF-B", "CHRW", "CA", "COG", "CPB", "COF", "CAH", "KMX", "CCL", "CAT", "CBG", "CBS", "CELG",
+            "CNC", "CNP", "CTL", "CERN", "CF", "SCHW", "CHK", "CVX", "CMG", "CB", "CHD", "CI", "XEC", "CINF", "CTAS",
+            "CSCO", "C", "CFG", "CTXS", "CME", "CMS", "COH", "CTSH", "CL", "CMCSA", "CMA", "CAG", "CXO", "COP", "ED",
+            "STZ", "GLW", "COST", "CCI", "CSRA", "CSX", "CMI", "CVS", "DHI", "DHR", "DRI", "DVA", "DE", "DLPH", "DAL",
+            "XRAY", "DVN", "DO", "DLR", "DFS", "DISCA", "DISCK", "DG", "DLTR", "D", "DOV", "DOW", "DPS", "DTE", "DD",
+            "DUK", "DNB", "ETFC", "EMN", "ETN", "EBAY", "ECL", "EIX", "EW", "EA", "EMC", "EMR", "ENDP", "ETR", "EOG",
+            "EQT", "EFX", "EQIX", "EQR", "ESS", "EL", "ES", "EXC", "EXPE", "EXPD", "ESRX", "EXR", "XOM", "FFIV", "FB",
+            "FAST", "FRT", "FDX", "FIS", "FITB", "FSLR", "FE", "FISV", "FLIR", "FLS", "FLR", "FMC", "FTI", "FL", "F",
+            "FTV", "FBHS", "BEN", "FCX", "FTR", "GPS", "GRMN", "GD", "GE", "GGP", "GIS", "GM", "GPC", "GILD", "GPN",
+            "GS", "GT", "GWW", "HAL", "HBI", "HOG", "HAR", "HRS", "HIG", "HAS", "HCA", "HCP", "HP", "HSIC", "HES",
+            "HPE", "HOLX", "HD", "HON", "HRL", "HST", "HPQ", "HUM", "HBAN", "ITW", "ILMN", "IR", "INTC", "ICE", "IBM",
+            "IP", "IPG", "IFF", "INTU", "ISRG", "IVZ", "IRM", "JBHT", "JEC", "JNJ", "JCI", "JPM", "JNPR", "KSU", "K",
+            "KEY", "KMB", "KIM", "KMI", "KLAC", "KSS", "KHC", "KR", "LB", "LLL", "LH", "LRCX", "LM", "LEG", "LEN",
+            "LUK", "LVLT", "LLY", "LNC", "LLTC", "LKQ", "LMT", "L", "LOW", "LYB", "MTB", "MAC", "M", "MNK", "MRO",
+            "MPC", "MAR", "MMC", "MLM", "MAS", "MA", "MAT", "MKC", "MCD", "MCK", "MJN", "MDT", "MRK", "MET", "KORS",
+            "MCHP", "MU", "MSFT", "MHK", "TAP", "MDLZ", "MON", "MNST", "MCO", "MS", "MSI", "MUR", "MYL", "NDAQ", "NOV",
+            "NAVI", "NTAP", "NFLX", "NWL", "NFX", "NEM", "NWSA", "NWS", "NEE", "NLSN", "NKE", "NI", "NBL", "JWN", "NSC",
+            "NTRS", "NOC", "NRG", "NUE", "NVDA", "ORLY", "OXY", "OMC", "OKE", "ORCL", "OI", "PCAR", "PH", "PDCO",
+            "PAYX", "PYPL", "PNR", "PBCT", "PEP", "PKI", "PRGO", "PFE", "PCG", "PM", "PSX", "PNW", "PXD", "PBI", "PNC",
+            "RL", "PPG", "PPL", "PX", "PCLN", "PFG", "PG", "PGR", "PLD", "PRU", "PEG", "PSA", "PHM", "PVH", "QRVO",
+            "QCOM", "PWR", "DGX", "RRC", "RTN", "O", "RHT", "REGN", "RF", "RSG", "RAI", "RHI", "ROK", "COL", "ROP",
+            "ROST", "RCL", "R", "SPGI", "CRM", "SCG", "SLB", "SNI", "STX", "SEE", "SRE", "SHW", "SIG", "SPG", "SWKS",
+            "SLG", "SJM", "SNA", "SO", "LUV", "SWN", "SE", "STJ", "SWK", "SPLS", "SBUX", "HOT", "STT", "SRCL", "SYK",
+            "STI", "SYMC", "SYF", "SYY", "TROW", "TGT", "TEL", "TGNA", "TDC", "TSO", "TXN", "TXT", "BK", "CLX", "KO",
+            "HSY", "MOS", "TRV", "DIS", "TMO", "TIF", "TWX", "TJX", "TMK", "TSS", "TSCO", "TDG", "RIG", "TRIP", "FOXA",
+            "FOX", "TYC", "TSN", "USB", "UDR", "ULTA", "UA", "UNP", "UAL", "UNH", "UPS", "URI", "UTX", "UHS", "UNM",
+            "URBN", "VFC", "VLO", "VAR", "VTR", "VRSN", "VRSK", "VZ", "VRTX", "VIAB", "V", "VNO", "VMC", "WMT", "WBA",
+            "WM", "WAT", "WFC", "HCN", "WDC", "WU", "WRK", "WY", "WHR", "WFM", "WMB", "WLTW", "WEC", "WYN", "WYNN",
+            "XEL", "XRX", "XLNX", "XL", "XYL", "YHOO", "YUM", "ZBH", "ZION", "ZTS"
+        ]
 
 def create_portfolio(user_id, portfolio_id, recommend_type='random', potential_stocks=None, num_observed_days=730, **kwargs):
 
@@ -39,7 +79,7 @@ def create_portfolio(user_id, portfolio_id, recommend_type='random', potential_s
     :param kwargs:
         dictionary, arguments for recommender algorithms. Keys can include
         budget, max_investment, time_horizon, diversity_threshold
-    :return: updates the database to include the user's portfolio
+    :return: a JSON Response of a dictionary with stockname -> amount to buy
     """
 
     # TODO: consider pulling data inside the recommender algorithm to avoid work for random recommender
@@ -48,10 +88,10 @@ def create_portfolio(user_id, portfolio_id, recommend_type='random', potential_s
     if recommend_type not in ['random', 'diverse', 'high_return']:
         raise ValueError('recommend_type not recognized')
 
-    # TODO: if potential stocks are not given, choose top k=500 popular stocks
+    # if potential stocks does not have anything, then choose from the S&P500
     if potential_stocks is None:
-        # potential_stocks = [] # hard-coded list
-        pass
+        SandPObject = SandP()
+        potential_stocks = SandPObject.stocks
 
     # from current time to number of observed days ago
     interfaceObject = stockDayDatabaseInterface()
@@ -71,6 +111,7 @@ def create_portfolio(user_id, portfolio_id, recommend_type='random', potential_s
 
 
     # run desired recommender algorithm
+    #note:  Portfolio is returned as a dictionary of stock name -> amount
     if recommend_type is 'random':
 
         current_stock_prices = stock_prices[:,0]
@@ -84,9 +125,5 @@ def create_portfolio(user_id, portfolio_id, recommend_type='random', potential_s
 
         portfolio = recommend_diverse_portfolio(stock_ids=stock_ids, stock_prices=stock_prices, **kwargs)
 
-    # TODO: update the database with the portfolio for the user
-    #Thoughts:  Is this necessary? Maybe we just want to return the array and have the stocks show up.
-
-    
-
-    return
+    #return a JSON compile of the recommended portfolio that front end code will deal with
+    return HttpResponse(json.dumps(portfolio), content_type="application/json")

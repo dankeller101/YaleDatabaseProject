46802585336ccb9b561d0e13793d94befd67f0b6
charshaw <charshaw@opendns.com>
2016-12-03 01:27:09 -0500

    presentation

---
+++ predict_my_money/computations/basic_computation_interface.py
@@ -0,0 +1,15 @@
+# basic_computations.py
+# December 2016
+# CPSC 537
+#
+# This file interfaces the front end user input to the backend, including
+# database queries and basic computation calls.
+
+def get_portfolio_tsr():
+
+    return
+
+
+def get_portfolio_diversity_score():
+
+    return
